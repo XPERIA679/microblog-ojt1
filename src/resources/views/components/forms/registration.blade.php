7090e6eddf4aa5f2a9f40af1a0a802db469ca323
janrebpol <revroaaa@gmail.com>
2024-04-04 11:34:18 +0800

    second commit feat/registration UI

--- src/resources/views/components/forms/registration.blade.php
+++ src/resources/views/components/forms/registration.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ url('assets/css/registration.css') }}">
-    <title>Registration</title>
+    <title>Sign Up</title>
 </head>
 
 
@@ -14,46 +14,85 @@
 
 
     <div class="container">
-        <div class="box form-box">
-            <form action="#">
-                <header style="text-align: center">
-                    <h2>Register</h2>
-                </header>
+        <div class="title">
+            <h3>Sign Up</h3>
+        </div>
 
-                <p>Enter the following:</p>
-                <div class="inputBox">
-                    <input type="text" id="email" required="required">
-                    <span>Email</span>
+        <form action="#">
+            <div class="user-details">
+
+                <div class="input-box ">
+                    <input type="text" placeholder="Juan" autocomplete="off" required="required">
+                    <span>First Name</span>
                 </div>
 
-                <div class="inputBox">
-                    <input type="text" required="required">
+                <div class="input-box">
+                    <input type="text" placeholder="Dela Cruz" autocomplete="off" required="required">
+                    <span>Last Name</span>
+                </div>
+
+                <div class="input-box">
+                    <input type="email" placeholder="example@gmail.com" autocomplete="off" required="required">
+                    <span>Email address</span>
+                </div>
+
+                <div class="input-box">
+                    <input type="text" placeholder="juandelacruz" autocomplete="off" required="required">
                     <span>Username</span>
                 </div>
 
-                <div class="inputBox">
+                <div class="input-box">
+                    <input type="tel" placeholder="09669513998" autocomplete="off" pattern="[0-9]{11}"
+                        required="required">
+                    <span>Phone Number</span>
+                </div>
+
+                <div class="input-box">
+                    <input type="text" placeholder="dd/mm/yyyy" onfocus="type='date'" onblur="type='text'"
+                        required="required">
+                    <span>Date of Birth</span>
+                </div>
+
+                <div class="input-box ">
+                    <input type="text" placeholder="blk# & lot#, Street" autocomplete="off" required="required">
+                    <span>Address</span>
+                </div>
+
+                <div class="input-box ">
+                    <input type="text" placeholder="City" autocomplete="off" required="required">
+                    <span>City</span>
+                </div>
+
+                <div class="input-box ">
+                    <input type="text" placeholder="State/Province" autocomplete="off" required="required">
+                    <span>State/Province</span>
+                </div>
+
+                <div class="input-box ">
+                    <input type="text" placeholder="ZIP/Postal Code" autocomplete="off" required="required">
+                    <span>ZIP/Postal Code</span>
+                </div>
+
+                <div class="input-box">
                     <input type="password" required="required">
                     <span>Password</span>
                 </div>
 
-                <div class="inputBox">
+                <div class="input-box">
                     <input type="password" required="required">
                     <span>Confirm Password</span>
-                </div><br>
-
-                <hr style="border-top: 2px solid #000">
-
-                <div>
-                    <button class="btn">
-                        Create Account
-                    </button>
                 </div>
 
                 <div>
-                    <a href="#">Already have an account? <br> Click here!</a>
+                    <button class="button">Create Account</button>
                 </div>
-            </form>
-        </div>
+
+                <div>
+                    <button class="button1">Already have an account?</button>
+                </div>
+            </div>
+        </form>
+    </div>
     </div>
 
 </body>

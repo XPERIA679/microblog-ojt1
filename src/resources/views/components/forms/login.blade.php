b2e547522f4f0c1645e432072805cd05bf23ba65
janrebpol <revroaaa@gmail.com>
2024-04-05 16:50:59 +0800

    Inital UI for User Login

--- src/resources/views/components/forms/login.blade.php
+++ src/resources/views/components/forms/login.blade.php
@@ -8,8 +8,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ url('assets/css/login.css') }}">
     <title>Login</title>
+    @vite(['resources/css/app.css', 'resources/scss/app.scss', 'resources/js/app.js'])
 </head>
 
 
@@ -31,9 +31,9 @@
                 </div>
 
                 <div class="input-box">
-                    <button class="button">Sign In</button>
+                    <button class="button" href="home">Sign In</button>
                     <hr style="width:40%;margin:10px;" >
-                    <button class="button1">Don't have an account?</button>
+                    <button class="button1" href="registration">Don't have an account?</button>
                 </div>
 
                 <div class="input-box">

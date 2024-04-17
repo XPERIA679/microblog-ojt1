015b9a3a9603bab58eb73dbac402efd064eed2c5
Lhio Labajo <labajolhio@gmail.com>
2024-04-17 17:34:47 +0800

    Initial UI for Landing Page

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    @include('layouts.head')
+    <title>@yield('title')</title>
+</head>
+<body>
+    <div id="app" class="">
+        <main>
+            @yield('content')
+        </main>
+    </div>
+    @include('layouts.scripts')
+</body>
+</html>
---
+++ src/tailwind.config.js
@@ -0,0 +1,61 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+],
+  theme: {
+    extend: {
+      colors: {
+        whiteish: '#dad2bc',
+        darkcolor: '#252323',
+        purewhite: '#f5f1ed',
+        mygray: '#70798c',
+      },
+      transitionProperty: {
+        'shadow-opacity-transform': 'box-shadow, opacity, transform',
+      },
+      transitionDuration: {
+        'tD': '280ms, 15ms, 270ms',
+      },
+      transitionTimingFunction: {
+        'tTF': 'cubic-bezier(.4, 0, .2, 1), linear, cubic-bezier(0, 0, .2, 1)',
+      },
+      transitionDelay: {
+        'tDelay': '0ms, 30ms, 0ms',
+      },
+      boxShadow: {
+        'boxshadow': 'rgba(0, 0, 0, .2) 0 3px 5px -1px, rgba(0, 0, 0, .14) 0 6px 10px 0, rgba(0, 0, 0, .12) 0 1px 18px 0',
+      },
+      backgroundImage: {
+        'gradient': 'linear-gradient(-60deg, #DAD2BC, #252323, #70798C)',
+      },
+      animation: {
+        'gradient': 'gradient 15s ease infinite',
+      },
+      keyframes: {
+        'gradient': {
+          '0%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+          '100%': { backgroundPosition: '0% 50%' },
+        }
+      },
+      backgroundSize: {
+        'bgbody': '500% 500%',
+      },
+      height: {
+        'body-height': '98vh',
+        'left-height': '100vh',
+      }
+    },
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif'],
+      heading: ['Poppins', 'sans-serif']
+    }
+  },
+  plugins: [
+    require('tailwindcss-gradients'),
+  ],
+}
+

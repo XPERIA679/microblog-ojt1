47bda456582d14b81d819a5e1025bc9f113664aa
janrebpol <revroaaa@gmail.com>
2024-04-17 10:15:06 +0800

    Initial Layouts for Registration

---
+++ src/resources/views/auth/signup.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+@section('title', 'Sign Up')
+
+@section('content')
+    <div class="form-container">
+        <div class="title">
+            <h3>Sign Up</h3>
+        </div>
+
+        <form action="#" id="signup-form">
+            <div class="user-details">
+
+                <div class="input-box">
+                    <input type="email" name="email" placeholder="example@gmail.com" autocomplete="off" required>
+                    <span>Email</span>
+                </div>
+
+                <div class="input-box">
+                    <input type="text" name="username" placeholder="juandelacruz" autocomplete="off" required>
+                    <span>Username</span>
+                </div>
+
+                <div class="input-box">
+                    <input type="password" name="password" required>
+                    <span>Password</span>
+                </div>
+
+                <div class="input-box">
+                    <input type="password" required>
+                    <span>Confirm Password</span>
+                </div>
+
+                <div class="input-box">
+                    <button class="button" href="home">Create Account</button>
+                </div>
+
+                <div class="input-box">
+                    <button class="button1" href="login">Already have an account?</button>
+                </div>
+
+            </div>
+        </form>
+    </div>
+@endsection

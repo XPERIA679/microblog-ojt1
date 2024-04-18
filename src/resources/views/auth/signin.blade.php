d42b119c89461ac69440cf5980bb6e361dfe1506
janrebpol <revroaaa@gmail.com>
2024-04-18 17:46:15 +0800

    Updated UI for Login

--- src/resources/views/auth/signin.blade.php
+++ src/resources/views/auth/signin.blade.php
@@ -3,26 +3,26 @@
 @section('title', 'Sign In')
 
 @section('content')
-    <div class="form-container">
+    <div class="form-container1">
         <div class="title">
-            <h3>Sign In</h3>
+            <h3 class="text-mywhite mix-blend-overlay">Sign In</h3>
         </div>
         <form action="#">
-            <div class="user-details">
-                <div class="input-box">
+            <div class="user-details2">
+                <div class="input-box2">
                     <input type="text" autocomplete="off" required="required">
                     <span>Username</span>
                 </div>
 
-                <div class="input-box">
+                <div class="input-box2">
                     <input type="password" required="required">
                     <span>Password</span>
                 </div>
 
-                <div class="input-box">
-                    <button class="button" href="home">Sign In</button>
+                <div class="input-box2">
+                    <button class="items-center text-mydark bg-mycream box-border cursor-pointer inline-flex text-sm font-medium h-12 max-w-full overflow-hidden relative text-center w-auto px-6 py-0.5 rounded-3xl hover:bg-mygray text-mycream focus:border-2 border-solid border-mydark" href="home">Sign In</button>
                     <hr class="w-2/5 m-2.5">
-                    <button class="button1" href="registration">Create Account</button>
+                    <button class="items-center text-mycream bg-mydark box-border cursor-pointer inline-flex text-sm font-medium h-12 max-w-full overflow-hidden relative text-center w-auto px-6 py-0.5 rounded-3xl hover:bg-mygray text-mycream focus:border-2 border-solid border-mydark" href="registration">Create Account</button>
                 </div>
             </div>
         </form>
--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,13 +1,52 @@
-/** @type {import('tailwindcss').Config} */
 export default {
     content: [
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
         "./resources/**/*.vue",
-      ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    ],
+    theme: {
+        extend: {
+            colors: {
+                mycream: '#dad2bc',
+                mydark: '#252323',
+                mywhite: '#f5f1ed',
+                mygray: '#70798c',
+            },
+            transitionProperty: {
+                'shadow-opacity-transform': 'box-shadow, opacity, transform',
+            },
+            transitionDuration: {
+                'transduration': '280ms, 15ms, 270ms',
+            },
+            transitionTimingFunction: {
+                'transtimefunc': 'cubic-bezier(.4, 0, .2, 1), linear, cubic-bezier(0, 0, .2, 1)',
+            },
+            transitionDelay: {
+                'transdelay': '0ms, 30ms, 0ms',
+            },
+            backgroundImage: {
+                'ngradient': 'linear-gradient(-60deg, #DAD2BC, #252323, #70798C)',
+                'pgradient': 'linear-gradient(60deg, #DAD2BC, #252323, #70798C)',
+            },
+            animation: {
+                'gradient': 'gradient 15s ease infinite',
+            },
+            keyframes: {
+                'gradient': {
+                    '0%': { backgroundPosition: '0% 50%' },
+                    '50%': { backgroundPosition: '100% 50%' },
+                    '100%': { backgroundPosition: '0% 50%' },
+                }
+            },
+            backgroundSize: {
+                'bgsize': '500% 500%',
+            },
+        },
+        fontFamily: {
+            sans: ['Roboto', 'sans-serif'],
+            heading: ['Poppins', 'sans-serif']
+        }
+    },
+    plugins: [
+    ],
 }
-

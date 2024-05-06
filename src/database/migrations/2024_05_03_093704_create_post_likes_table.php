87b94a784e1312d6502b5861c521bbf28ec96b5c
jeromes123 <qjjpsoriano@tip.edu.ph>
2024-05-06 13:23:30 +0800

    Edited migration for post likes table, added softdeletes

--- src/database/migrations/2024_05_03_093704_create_post_likes_table.php
+++ src/database/migrations/2024_05_03_093704_create_post_likes_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->foreign('post_id')->references('id')->on('user_posts')->onDelete('cascade');
             $table->timestamps(); 
             $table->unique(['user_id', 'post_id']);
+            $table->softDeletes();
         });
     }
 

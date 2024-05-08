10ffce6d4610146fda0298b49d45f6518546953a
XPERIA679 <sorianorobin38@gmail.com>
2024-05-08 10:47:57 +0800

    Remove Comments from script

--- src/public/js/email.js
+++ src/public/js/email.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     var button = document.querySelector("button[type='submit']");
     var countdownDisplay = document.getElementById("countdown");
 
-    button.disabled = true; // Initially disable the button
+    button.disabled = true;
     var countdownSeconds = 60;
     countdownDisplay.textContent = countdownSeconds;
 
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function() {
         countdownDisplay.textContent = countdownSeconds;
         if (countdownSeconds <= 0) {
             clearInterval(countdownInterval);
-            button.disabled = false; // Enable the button after countdown finishes
+            button.disabled = false;
         }
-    }, 1000); // Update every second
+    }, 1000);
 });

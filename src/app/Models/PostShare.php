b36d44adbad84c04b058ef3f2e6d8eab151b4808
jeromes123 <qjjpsoriano@tip.edu.ph>
2024-05-10 10:55:16 +0800

    created migration and model for shares

---
+++ src/app/Models/PostShare.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class PostShare extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'post_id',
+        'user_id',
+        'repost_content',
+    ];
+
+    public function post(): BelongsTo 
+    {
+        return $this->belongsTo(UserPost::class, 'post_id');
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}

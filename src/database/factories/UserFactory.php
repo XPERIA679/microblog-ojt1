f1bf9b2013d266ba2fedf0ea7b67257ee9c273c4
jeromes123 <qjjpsoriano@tip.edu.ph>
2024-05-22 11:15:53 +0800

    created factory for user and post

--- src/database/factories/UserFactory.php
+++ src/database/factories/UserFactory.php
@@ -18,17 +18,14 @@ class UserFactory extends Factory
 
     /**
      * Define the model's default state.
-     *
-     * @return array<string, mixed>
      */
-    public function definition(): array
+    public function definition(): array 
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'username' => $this->faker->userName,
+            'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'password' => bcrypt('Qq1234567'),
         ];
     }
 

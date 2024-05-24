ef1be4fb997c89e8c4f3d16cd7800d8b814c313c
jeromes123 <qjjpsoriano@tip.edu.ph>
2024-05-24 09:31:14 +0800

    corrected the referenced table name

--- src/database/migrations/2024_04_30_084108_create_post_media_table.php
+++ src/database/migrations/2024_04_30_084108_create_post_media_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('post_media', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('post_id')->constrained('posts')->onDelete('cascade');
+            $table->foreignId('post_id')->constrained('user_posts')->onDelete('cascade');
             $table->mediumText('image');
             $table->timestamps();
         });

6758f64c5db0c8bf6acfbdda3676c958d7e4b81f
Lhio Labajo <labajolhio@gmail.com>
2024-05-28 15:46:01 +0800

    Fixed Login Error UI

--- src/resources/views/components/errors/login-first.blade.php
+++ src/resources/views/components/errors/login-first.blade.php
@@ -1,14 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Please Login First</title>
-</head>
-<body>
-    <h1>Oops! Please Login First</h1>
-    <p>To do that, you need to be logged in.</p>
-    <p>Please <a href="/login">login</a> or <a href="/register">create an account</a> if you don't have one.</p>
-    <p><a href="">Go back</a></p>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('title', 'Please Login First')
+
+@section('content')
+
+<div class="flex justify-center items-center h-left-height m-0">
+    <div class="box-container text-center border bg-mywhite shadow-custom-shadowbox text-mydark p-5 rounded-lg border-solid border-mygray">
+        <h1 class="text-2xl font-semibold text-mydark mb-5">Oops! Please Login First</h1><br>
+        <div>
+            <p class="text-base text-mydark mx-0 my-2.5">To do that, you need to be logged in.</p>
+            <p class="text-base text-mydark mx-0 my-2.5">Please <a href="/signin" class="text-blue-600 underline">Login</a> or <a href="/signup" class="text-blue-600 underline">Create an account</a> if you dont have one.</p><br>
+            <p class="text-base text-mydark mx-0 my-2.5"><a href="javascript:history.back()" class="text-blue-600 underline">Go back</a></p>
+        </div>
+    </div>
+
+</div>
+
+@endsection

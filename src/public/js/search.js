ed1bd769ef67affa6571e612f709edae0afb1403
Lhio Labajo <labajolhio@gmail.com>
2024-05-31 16:01:53 +0800

    Resolved else in the if statement

--- src/public/js/search.js
+++ src/public/js/search.js
@@ -2,30 +2,32 @@ async function filterUsernames() {
     const query = document.getElementById('search-bar').value;
     const dropdown = document.getElementById('dropdown');
 
-    if (query.length > 0) {
-        const response = await fetch(`/api/usernames?query=${query}`);
-        const usernames = await response.json();
-
-        dropdown.innerHTML = '';
-        if (usernames.length > 0) {
-            usernames.forEach(username => {
-                const div = document.createElement('div');
-                div.className = 'dropdown-item p-2 hover:bg-mycream';
-                div.textContent = username;
-                div.addEventListener('click', () => {
-                    console.log(`Username clicked: ${username}`);
-                    document.getElementById('search-bar').value = username;
-                    dropdown.classList.add('hidden');
-                });
-                dropdown.appendChild(div);
-            });
-            dropdown.classList.remove('hidden');
-        } else {
-            dropdown.classList.add('hidden');
-        }
-    } else {
+    if (query.length === 0) {
         dropdown.classList.add('hidden');
+        return;
     }
+
+    const response = await fetch(`/api/usernames?query=${query}`);
+    const usernames = await response.json();
+
+    dropdown.innerHTML = '';
+    if (usernames.length === 0) {
+        dropdown.classList.add('hidden');
+        return;
+    }
+
+    usernames.forEach(username => {
+        const div = document.createElement('div');
+        div.className = 'dropdown-item p-2 hover:bg-mycream';
+        div.textContent = username;
+        div.addEventListener('click', () => {
+            console.log(`Username clicked: ${username}`);
+            document.getElementById('search-bar').value = username;
+            dropdown.classList.add('hidden');
+        });
+        dropdown.appendChild(div);
+    });
+    dropdown.classList.remove('hidden');
 }
 
 document.getElementById('search-bar').addEventListener('focus', () => {

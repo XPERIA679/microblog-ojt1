c55fb67d10c3e6094c0e334d866c08c061f97f25
janrebpol <revroaaa@gmail.com>
2024-05-31 11:42:22 +0800

    Profile Page

---
+++ src/resources/views/profile/index.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('title', 'My Thoughts')
+
+@section('content')
+
+    <div class="bg-mycream bg-opacity-0 relative w-full h-full justify-center items-center transition-opacity duration-500">
+        <main class="grid lg:grid-cols-3 gap-6 my-12 mx-12 w-2xl p-10 justify-center relative">
+            <x-modals.edit-post />
+            <x-modals.edit-profile />
+            <x-modals.create-comment />
+            <x-modals.share-post />
+            <section class="lg:col-span-1 px-6">
+                <div class="rounded-lg p-10 bg-mydark">
+                    <x-profile-icon.big />
+                    <div class="flex justify-center items-center gap-6 my-5">
+                        <x-counter.post />
+                        <x-counter.follower />
+                        <x-counter.following />
+                    </div>
+                    <div class="flex justify-center items-center">
+                        <x-buttons.edit-profile />
+                    </div>
+                </div>
+                <x-sections.follow-suggestions />
+            </section>
+            <section class="lg:col-span-2 ">
+                <x-forms.create-post />
+                <x-sections.post />
+            </section>
+        </main>
+    </div>
+
+@endsection

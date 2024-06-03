55038bef501f5978f5daba9d19851cc77d444691
janrebpol <revroaaa@gmail.com>
2024-06-03 11:14:18 +0800

    Email Verification

---
+++ src/resources/views/components/buttons/resend-btn.blade.php
@@ -0,0 +1,6 @@
+<div class="flex justify-center items-center mt-5">
+    <button type="submit"
+        class="flex justify-center items-center text-mydark bg-mycream text-sm lg:text-base font-semibold h-12 w-full overflow-hidden lg:w-80 px-6 py-1 rounded-3xl hover:bg-mywhite transition-all">
+        Resend Email Verification
+    </button>
+</div>

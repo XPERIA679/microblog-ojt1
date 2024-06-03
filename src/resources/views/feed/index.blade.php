db78c205e91e3d5d1808d57fd695d68efa8d9dac
XPERIA679 <sorianorobin38@gmail.com>
2024-06-03 17:40:46 +0800

    Fix Modal Functionality

--- src/resources/views/feed/index.blade.php
+++ src/resources/views/feed/index.blade.php
@@ -7,8 +7,7 @@
     <div class="bg-mycream bg-opacity-0 relative w-full h-full justify-center items-center transition-opacity duration-500">
         <main class="grid lg:grid-cols-3 gap-6 my-12 mx-12 w-2xl p-10 justify-center relative">
             <x-modals.edit-profile />
-            <x-modals.create-comment-modal :postsMediumOrShare="$postsMediumOrShare"/>
-            <x-modals.share-post />
+
             <section class="lg:col-span-1 px-6">
                 <div class="rounded-lg p-10 bg-mydark">
                     <x-profile-icon.big />
@@ -26,8 +25,9 @@
             <section class="lg:col-span-2 ">
                 <x-forms.create-post />
                 @foreach ($postsMediaAndShares as $postsMediumOrShare)
+                <x-modals.create-comment-modal :postsMediumOrShare="$postsMediumOrShare"/>
                 <x-modals.edit-post />
-                <x-modals.create-comment />
+                <x-modals.create-comment-modal :postsMediumOrShare="$postsMediumOrShare" />
                 <x-modals.share-post :postsMediumOrShare="$postsMediumOrShare"/>
                 <x-sections.post :postsMediumOrShare="$postsMediumOrShare"/>
                 @endforeach

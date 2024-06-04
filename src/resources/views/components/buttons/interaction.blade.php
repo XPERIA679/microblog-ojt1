109de154fc6c8f082ef64b291e7e475da3c86500
XPERIA679 <sorianorobin38@gmail.com>
2024-06-04 17:23:44 +0800

    Integrated Like and Unlike function on Posts in Newsfeed

--- src/resources/views/components/buttons/interaction.blade.php
+++ src/resources/views/components/buttons/interaction.blade.php
@@ -1,8 +1,29 @@
 <div class="grid grid-cols-3 justify-center items-center divide-x-2 divide-mydark divide-solid">
-    <button
+    @php
+        $post = $postsMediumOrShare instanceof App\Models\PostShare ? $postsMediumOrShare->post : $postsMediumOrShare['post'];
+    @endphp
+    @if (in_array(auth()->id(), $post->postLike->pluck('user_id')->toArray()))
+    <form action="/unlike-post" method="POST">
+        @csrf
+        @method('DELETE')
+        <input type="text" name="type" value="{{ $post instanceof App\Models\PostShare ? 'share' : 'originalPost' }}" hidden>
+        <input type="text" name="id" value="{{ $post->id }}" hidden>
+    <button href="submit"
+        class="col-span-1 hover:bg-mydark hover:text-mycream font-semibold justify-center items-center p-2 transition-all focus:bg-mydark focus:text-mycream rounded">
+        Unlike
+    </button>
+    </form>
+    @else
+    <form action="/like-post" method="POST">
+        @csrf
+    <input type="text" name="type" value="{{ $post instanceof App\Models\PostShare ? 'share' : 'originalPost' }}" hidden>
+    <input type="text" name="id" value="{{ $post->id }}" hidden>
+    <button href="submit"
         class="col-span-1 hover:bg-mydark hover:text-mycream font-semibold justify-center items-center p-2 transition-all focus:bg-mydark focus:text-mycream rounded">
         Like
     </button>
+    </form>
+    @endif
     <button onclick="showComment({{ $postsMediumOrShare['post']->id }})"
         class="col-span-1 hover:bg-mydark hover:text-mycream font-semibold justify-center items-center p-2 transition-all rounded">
         Comment

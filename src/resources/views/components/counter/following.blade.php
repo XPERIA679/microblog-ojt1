e6d6a42e6f22de4b76f4b200dda6cfefa4b85262
jeromes123 <qjjpsoriano@tip.edu.ph>
2024-06-05 22:32:48 +0800

    integrated backend to follow-suggestions section and counters

--- src/resources/views/components/counter/following.blade.php
+++ src/resources/views/components/counter/following.blade.php
@@ -1,4 +1,4 @@
 <div class="font-semibold text-center">
-    <p class="text-mywhite cursor-pointer hover:text-mygray">1</p>
+    <p class="text-mywhite cursor-pointer hover:text-mygray">{{ auth()->user()->followedUsers->count() }}</p>
     <span class="text-mycream cursor-pointer hover:text-mygray">Following</span>
 </div>
--- src/resources/views/components/sections/follow-suggestions.blade.php
+++ src/resources/views/components/sections/follow-suggestions.blade.php
@@ -1,9 +1,18 @@
+@php
+    $authUserId = auth()->user()->id;
+    $unfollowedUsers = 
+        App\Models\User::whereKeyNot($authUserId)
+        ->whereNotIn('id', auth()->user()->followedUsers->pluck('following_id'))
+        ->get();
+@endphp
+
 <div class="rounded-lg my-5 p-6 bg-mydark h-96 overflow-scroll overflow-x-hidden">
+    @foreach($unfollowedUsers as $unfollowedUser)
     <div class="flex flex-row px-2 pb-2 pt-4 my-3 w-auto">
         <x-profile-icon.small />
         <div class="flex flex-col my-2 ml-4 pr-12">
             <div class="text-mycream text-sm font-semibold cursor-pointer">
-                Moon
+                {{$unfollowedUser->username}}
             </div>
             <div class="text-mywhite flex font-light text-xs">
                 33k followers
@@ -17,4 +26,5 @@
             </button>
         </div>
     </div>
+    @endforeach
 </div>

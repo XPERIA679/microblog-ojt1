d58f75c16d59dbd9c45c34a28069ff789796e940
janrebpol <revroaaa@gmail.com>
2024-06-05 16:15:45 +0800

    fixed bug on edit profile modal

--- src/resources/views/components/modals/edit-profile.blade.php
+++ src/resources/views/components/modals/edit-profile.blade.php
@@ -5,7 +5,7 @@
 @endphp
 
 <div onclick="hideProfile()" id="profile"
-    class="fixed left-0 top-0 bg-mydark bg-opacity-50 w-full h-full justify-center items-center opacity-0 hidden transition-opacity duration-500">
+    class="fixed left-0 top-0 bg-mydark bg-opacity-50 w-full h-full justify-center items-center opacity-0 hidden transition-opacity duration-500 z-50">
     <div onclick="event.stopImmediatePropagation()" class="bg-mycream rounded-2xl shadow-md p-10 flex">
         <form method="POST" action="{{ route('update-profile') }}">
         @csrf
@@ -15,11 +15,11 @@
                 <h1 class="left-0 text-3xl font-bold text-mydark">
                     Profile Information
                 </h1>
-                <div class="grid grid-cols-2 gap-6 mt-8"> 
+                <div class="grid grid-cols-2 gap-6 mt-8">
                     <div class="relative">
                         <input id="first_name" name="first_name" type="text"
                             class="peer h-10 w-full bg-mycream border-b border-mygray text-mydark placeholder-transparent focus:outline-none focus:border-mydark"
-                            placeholder="First Name" autocomplete="off" 
+                            placeholder="First Name" autocomplete="off"
                             value="{{ $profile->first_name }}"/>
                         <label for="first_name"
                             class="absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">
@@ -29,7 +29,7 @@
                     <div class="relative">
                         <input id="lastname" type="text" name="last_name"
                             class="peer h-10 w-full bg-mycream border-b border-mygray text-mydark placeholder-transparent focus:outline-none focus:border-mydark"
-                            placeholder="Last Name" autocomplete="off" 
+                            placeholder="Last Name" autocomplete="off"
                             value="{{ $profile->last_name }}"/>
                         <label for="last_name"
                             class="absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">
@@ -39,7 +39,7 @@
                     <div class="relative">
                         <input id="bday" type="date" name="birth_date"
                             class="peer h-10 w-full bg-mycream border-b border-mygray text-mydark placeholder-transparent focus:outline-none focus:border-mydark"
-                            placeholder="Birthdate" autocomplete="off" 
+                            placeholder="Birthdate" autocomplete="off"
                             value="{{ $profile->birth_date }}"/>
                         <label for="birth_date"
                             class="absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">
@@ -49,7 +49,7 @@
                     <div class="relative">
                         <input id="contact" type="text" name="contact"
                             class="peer h-10 w-full bg-mycream border-b border-mygray text-mydark placeholder-transparent focus:outline-none focus:border-mydark"
-                            placeholder="Phone Number" autocomplete="off" 
+                            placeholder="Phone Number" autocomplete="off"
                             value="{{ $profile->contact }}"/>
                         <label for="contact"
                             class="absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">
@@ -76,7 +76,7 @@
                     <div class="relative">
                         <input id="lotBlk" name="lotBlk" type="text"
                             class="peer h-10 w-full bg-mycream border-b border-mygray text-mydark placeholder-transparent focus:outline-none focus:border-mydark"
-                            placeholder="Lot & Blk" autocomplete="off" 
+                            placeholder="Lot & Blk" autocomplete="off"
                             value="{{ $addressInfo['lotBlk'] }}"/>
                         <label for="lotBlk"
                             class="absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">
@@ -86,7 +86,7 @@
                     <div class="relative">
                         <input id="street" name="street" type="text"
                             class="peer h-10 w-full bg-mycream border-b border-mygray text-mydark placeholder-transparent focus:outline-none focus:border-mydark"
-                            placeholder="Street" autocomplete="off" 
+                            placeholder="Street" autocomplete="off"
                             value="{{ $addressInfo['street'] }}"/>
                         <label for="street"
                             class="absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">
@@ -96,7 +96,7 @@
                     <div class="relative">
                         <input id="city" name="city" type="text"
                             class="peer h-10 w-full bg-mycream border-b border-mygray text-mydark placeholder-transparent focus:outline-none focus:border-mydark"
-                            placeholder="City" autocomplete="off" 
+                            placeholder="City" autocomplete="off"
                             value="{{ $addressInfo['city'] }}"/>
                         <label for="city"
                             class="absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">
@@ -106,7 +106,7 @@
                     <div class="relative">
                         <input id="province" name="province" type="text"
                             class="peer h-10 w-full bg-mycream border-b border-mygray text-mydark placeholder-transparent focus:outline-none focus:border-mydark"
-                            placeholder="Province" autocomplete="off" 
+                            placeholder="Province" autocomplete="off"
                             value="{{ $addressInfo['province'] }}"/>
                         <label for="province"
                             class="absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">
@@ -116,7 +116,7 @@
                     <div class="relative">
                         <input id="country" name="country" type="text"
                             class="peer h-10 w-full bg-mycream border-b border-mygray text-mydark placeholder-transparent focus:outline-none focus:border-mydark"
-                            placeholder="Country" autocomplete="off" 
+                            placeholder="Country" autocomplete="off"
                             value="{{ $addressInfo['country'] }}"/>
                         <label for="country"
                             class="absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">
@@ -126,7 +126,7 @@
                     <div class="relative">
                         <input id="zip" name="zip" type="text"
                             class="peer h-10 w-full bg-mycream border-b border-mygray text-mydark placeholder-transparent focus:outline-none focus:border-mydark"
-                            placeholder="Zip Code" autocomplete="off" 
+                            placeholder="Zip Code" autocomplete="off"
                             value="{{ $addressInfo['zip'] }}"/>
                         <label for="zip"
                             class="absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">

598ccac52b83371fc071253fb88a4643d22a8cb1
Lhio Labajo <labajolhio@gmail.com>
2024-06-06 15:31:04 +0800

    Navbar Bugfix

--- src/resources/views/components/modals/share-post.blade.php
+++ src/resources/views/components/modals/share-post.blade.php
@@ -1,23 +1,18 @@
-<div onclick="hideUserPost({{ $postsMediumOrShare['post']->id }})" id="sharepost-{{ $postsMediumOrShare['post']->id }}"
-    class="fixed left-0 top-0 bg-mydark bg-opacity-50 w-full h-full justify-center items-center opacity-0 hidden transition-opacity duration-500">
-    <div onclick="event.stopImmediatePropagation()" class="bg-mycream rounded-2xl shadow-md w-auto h-auto">
+<div id="sharepost-{{ $postsMediumOrShare['post']->id }}" class="fixed left-0 top-0 bg-mydark bg-opacity-50 w-full h-full justify-center items-center opacity-0 hidden transition-opacity duration-500 z-9999">
+    <div onclick="event.stopImmediatePropagation()" class="bg-mycream rounded-2xl shadow-md w-11/12 max-w-lg max-h-[calc(100vh-50px)] overflow-y-auto">
         <form action="/share-post" method="POST">
             @csrf
-        <div class="flex flex-row mx-2 w-auto">
-            <div class="w-auto h-auto p-2 rounded-full">
-                <img class="w-8 h-8 m-1 object-cover rounded-full shadow cursor-pointer" src="assets/images/moon.jpg"
-                    alt="moon">
+            <div class="flex flex-row mx-2 w-auto">
+                <div class="w-auto h-auto p-2 rounded-full">
+                    <img class="w-8 h-8 m-1 object-cover rounded-full shadow cursor-pointer" src="assets/images/moon.jpg" alt="moon">
+                </div>
+                <input type="text" name="post_id" value="{{$postsMediumOrShare['post']->id}}" hidden>
+                <input type="text" name="user_id" value="{{auth()->id()}}" hidden>
+                <textarea name="repost_content" maxlength="45" rows="2" class="bg-mycream p-2 w-11/12 focus:outline-none text-md m-2 font-medium text-mydark rounded-2xl placeholder-mygray resize-none overflow-x-hidden"></textarea>
+                <button class="flex items-center py-2 px-4 rounded-lg text-sm text-mydark transition-all">
+                    <x-svgs.post-icon />
+                </button>
             </div>
-            <input type="text" name= "post_id" value="{{$postsMediumOrShare['post']->id}}" hidden>
-            <input type="text" name= "user_id" value="{{auth()->id()}}" hidden>
-            <textarea name="repost_content" maxlength="45" rows="2"
-                class="bg-mycream p-2 w-11/12 focus:outline-none text-md m-2 font-medium text-mydark rounded-2xl placeholder-mygray resize-none overflow-x-hidden">
-            </textarea>
-            <button
-                class="flex items-center py-2 px-4 rounded-lg text-sm text-mydark transition-all">
-                <x-svgs.post-icon />
-            </button>
-        </div>
         </form>
         <div class="border shadow-lg border-solid border-opacity-20 border-mygray rounded-2xl">
             <x-sections.post :postsMediumOrShare="$postsMediumOrShare" :showInteraction="false" :showTimerAndEdit="false"/>
--- src/resources/views/layouts/navbar.blade.php
+++ src/resources/views/layouts/navbar.blade.php
@@ -1,22 +1,21 @@
-<header class="bg-mydark text-white p-1 flex justify-between items-center h-50px z-1000 top-0 w-full fixed rounded-br-20px rounded-bl-20px border-t-0 border-2 border-solid border-mygray">
-    <a href=""><img src="assets/images/logo.png" alt="Logo" class="shrink-0 justify-center items-center max-w-[100px] h-[50px] mr-auto py-2"></a>
+<header class="bg-mydark text-white p-1 flex justify-between items-center z-1000 top-0 left-0 w-full fixed rounded-br-20px rounded-bl-20px border-t-0 border-2 border-solid border-mygray">
+    <a href=""><img src="assets/images/logo.png" alt="Logo" class="shrink-0 justify-center items-center max-w-[100px] h-[50px] pl-8 mr-auto py-2"></a>
 
     <div class="search-bar-container relative ml-0.5">
         <div class="search-container bg-mywhite rounded-3xl px-5 py-1 flex items-center relative transition-transform duration-300 ease-in-out">
             <i class="uil uil-search"></i>
             <input type="search" placeholder="Search..." id="search-bar" onkeyup="filterUsernames()" class="bg-transparent w-60 text-center h-6 text-xs text-mydark outline-none focus:scale-105">
-
             <div id="dropdown" class="absolute bg-mywhite top-full left-0 w-full border h-10 rounded-custom max-h-48 overflow-y-auto text-xs shadow-custom-boxshadow z-1000 pt-0.5 pl-2.5 cursor-pointer text-mydark hover:bg-mycream hidden"></div>
         </div>
     </div>
 
     <ul>
-        <li class="relative">
+        <li class="relative pr-8">
             <div id="profile-image-container" class="profile-image-container overflow-hidden shadow-custom-boxshadow cursor-pointer">
                 <img src="profile.jpg" alt="Profile Picture" class="w-10 h-10 rounded-b-half border-2 border-solid border-mywhite object-cover">
             </div>
 
-            <div id="dropdown-menu" class="dropdown-menu hidden absolute right-0 mt-1 w-24 bg-mywhite rounded-xl shadow-lg z-10 text-center">
+            <div id="dropdown-menu" class="dropdown-menu hidden absolute right-0 mt-1 mr-2 w-24 bg-mywhite rounded-xl shadow-lg z-10 text-center">
                 <ul>
                     <li class="p-0.5 flex items-center justify-center h-8 text-mydark text-xs bg-mywhite hover:bg-mycream cursor-pointer rounded-md">Profile</li>
                     <li class="p-0.5 flex items-center justify-center h-8 text-mydark text-xs bg-mywhite hover:bg-mycream cursor-pointer rounded-md" id="logout-button">Log Out</li>

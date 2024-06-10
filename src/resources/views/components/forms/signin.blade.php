b16e85af977d70619e0e4afbd01c3ae1c4440783
jeromes123 <qjjpsoriano@tip.edu.ph>
2024-06-10 14:54:02 +0800

    removed duplicated error messages

--- src/resources/views/components/forms/signin.blade.php
+++ src/resources/views/components/forms/signin.blade.php
@@ -1,6 +1,26 @@
 <form class="grid grid-cols-1 gap-6 mt-12" method="POST" action="/login">
     @csrf
 
+    @if($errors->has('loginUsername'))
+        @error('loginUsername')
+            <div class="peer h-10 w-full bg-mydark border-b border-mywhite text-red-500 placeholder-transparent focus:outline-none focus:border-mycream">
+                {{ $message }}
+            </div>
+        @enderror
+    @elseif($errors->has('loginPassword'))
+        @error('loginPassword')
+            <div class="peer h-10 w-full bg-mydark border-b border-mywhite text-red-500 placeholder-transparent focus:outline-none focus:border-mycream">
+                {{ $message }}
+            </div>
+        @enderror
+    @elseif($errors->has('loginUsername') && $errors->has('loginPassword'))
+        @error('loginPassword')
+            <div class="peer h-10 w-full bg-mydark border-b border-mywhite text-red-500 placeholder-transparent focus:outline-none focus:border-mycream">
+                {{ $message }}
+            </div>
+        @enderror
+    @endif
+
     <div class="relative my-3">
         <input name="loginUsername" value="{{ old('loginUsername') }}" type="text" maxlength="20"
             class="peer h-10 w-full bg-mydark border-b border-mywhite text-mycream placeholder-transparent focus:outline-none focus:border-mycream"
@@ -9,9 +29,6 @@
             class="absolute left-0 -top-3.5 text-mycream text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-mywhite peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-mycream peer-focus:text-sm">
             Username
         </label>
-        @error('loginUsername')
-            <div class="text-red-500 text-xs">{{ $message }}</div>
-        @enderror
     </div>
 
     <div class="relative my-3">
@@ -22,9 +39,6 @@
             class="absolute left-0 -top-3.5 text-mycream text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-mywhite peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-mycream peer-focus:text-sm">
             Password
         </label>
-        @error('loginPassword')
-            <div class="text-red-500 text-xs">{{ $message }}</div>
-        @enderror
     </div>
 
     <div class="mt-12 justify-center flex">

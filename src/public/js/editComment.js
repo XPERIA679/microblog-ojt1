2d6e90df27c5d720ec1a6eac30d9d2fd0c3a4f70
janrebpol <revroaaa@gmail.com>
2024-06-13 17:38:59 +0800

    Update edit comment modal

--- src/public/js/editComment.js
+++ src/public/js/editComment.js
@@ -1,9 +1,21 @@
 function editComment(commentId, content) {
+    const editCommentModal = document.getElementById('editCommentModal');
     document.getElementById('postCommentToEditId').value = commentId;
     document.getElementById('editCommentContent').value = content;
-    document.getElementById('editCommentModal').classList.remove('hidden');
+    editCommentModal.classList.remove("hidden");
+    editCommentModal.classList.add("flex");
+    setTimeout(() => {
+        editCommentModal.classList.add("opacity-100");
+        editCommentModal.classList.remove("opacity-0");
+    }, 20);
 }
 
 function closeEditModal() {
-    document.getElementById('editCommentModal').classList.add('hidden');
+    const editCommentModal = document.getElementById('editCommentModal');
+    editCommentModal.classList.add("opacity-0");
+    editCommentModal.classList.remove("opacity-100");
+    setTimeout(() => {
+        editCommentModal.classList.add("hidden");
+        editCommentModal.classList.remove("flex");
+    }, 500);
 }
--- src/resources/views/components/forms/create-comment-form.blade.php
+++ src/resources/views/components/forms/create-comment-form.blade.php
@@ -1,21 +1,14 @@
 @php
-    $postId = $postsMediumOrShare instanceof App\Models\PostShare 
-        ? $postsMediumOrShare->id
-        : $postsMediumOrShare['post']->id;
-    $idField = $postsMediumOrShare instanceof App\Models\PostShare 
-        ? 'post_share_id'
-        : 'post_id'
+    $postId =
+        $postsMediumOrShare instanceof App\Models\PostShare ? $postsMediumOrShare->id : $postsMediumOrShare['post']->id;
+    $idField = $postsMediumOrShare instanceof App\Models\PostShare ? 'post_share_id' : 'post_id';
 @endphp
 <form class="bg-mycream p-4 items-center" method="POST" action="{{ route('postComment.create') }}">
     @csrf
     <textarea maxlength="90" rows="3" placeholder="Thoughts about this post..."
-        class="w-full rounded-lg p-2 text-sm bg-mycream border hover:drop-shadow-md rounded-tg placeholder-mygray resize-none overflow-x-hidden"
+        class="w-full rounded-lg p-2 text-sm bg-mycream border hover:drop-shadow-md rounded-tg focus:outline-none placeholder-mygray resize-none overflow-x-hidden"
         autofocus name="content"></textarea>
-    <input name="user_id" value = {{ auth()->id() }} hidden>
-    <input name="{{$idField}}" value = "{{ $postId }}" hidden>
-    <button
-        class="flex items-center py-2 px-4 rounded-lg text-sm hover:bg-mydark hover:text-mycream bg-mywhite text-mydark shadow-lg transition-all">
-        Comment
-        <x-svgs.post-icon />
-    </button>
+    <input name="user_id" value={{ auth()->id() }} hidden>
+    <input name="{{ $idField }}" value = "{{ $postId }}" hidden>
+    <x-buttons.comment-btn />
 </form>

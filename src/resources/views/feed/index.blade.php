add5c15ea79ddf10b704f0146c45765df2ef5a44
janrebpol <revroaaa@gmail.com>
2024-06-13 18:01:05 +0800

    margin removed

--- src/resources/views/feed/index.blade.php
+++ src/resources/views/feed/index.blade.php
@@ -4,22 +4,22 @@
 
 @section('content')
 
-@extends('layouts.navbar')
+    @extends('layouts.navbar')
 
     <div class="bg-mycream bg-opacity-0 relative w-full h-full justify-center items-center transition-opacity duration-500">
-        <main class="grid lg:grid-cols-3 gap-6 my-12 mx-12 w-2xl p-10 justify-center relative">
+        <main class="grid lg:grid-cols-3 gap-6 mx-12 w-2xl p-10 justify-center relative">
             <x-modals.edit-profile />
-            <x-modals.follower :user="auth()->user()"/>
-            <x-modals.following :user="auth()->user()"/>
+            <x-modals.follower :user="auth()->user()" />
+            <x-modals.following :user="auth()->user()" />
             <x-modals.delete-post />
 
-            <section class="lg:col-span-1 px-6 lg:sticky top-12 self-start my-4 overflow-hidden">
+            <section class="lg:col-span-1 px-6 lg:sticky top-12 self-start overflow-hidden">
                 <div class="rounded-lg p-10 bg-mydark mt-8">
-                    <x-profile-icon.big :user="auth()->user()"/>
+                    <x-profile-icon.big :user="auth()->user()" />
                     <div class="flex justify-center items-center gap-6 my-5">
-                        <x-counter.post :user="auth()->user()"/>
-                        <x-counter.follower :user="auth()->user()"/>
-                        <x-counter.following :user="auth()->user()"/>
+                        <x-counter.post :user="auth()->user()" />
+                        <x-counter.follower :user="auth()->user()" />
+                        <x-counter.following :user="auth()->user()" />
                     </div>
                     <div class="flex justify-center items-center">
                         <x-buttons.edit-profile />
@@ -31,16 +31,16 @@
                 <x-notifications.notification-message />
                 <x-forms.create-post />
                 @foreach ($postsMediaAndShares as $postsMediumOrShare)
-                    <x-modals.create-comment-modal :postsMediumOrShare="$postsMediumOrShare"/>
-                    <x-modals.edit-post :postsMediumOrShare="$postsMediumOrShare"/>
-                    <x-modals.share-post :postsMediumOrShare="$postsMediumOrShare"/>
-                    <x-sections.post :postsMediumOrShare="$postsMediumOrShare"/>
+                    <x-modals.create-comment-modal :postsMediumOrShare="$postsMediumOrShare" />
+                    <x-modals.edit-post :postsMediumOrShare="$postsMediumOrShare" />
+                    <x-modals.share-post :postsMediumOrShare="$postsMediumOrShare" />
+                    <x-sections.post :postsMediumOrShare="$postsMediumOrShare" />
                 @endforeach
-                <div class="mt-4">
-                    {{ $postsMediaAndShares->links('vendor.pagination.feed') }}
-                </div>
             </section>
         </main>
+    <div class="mt-px">
+        {{ $postsMediaAndShares->links('vendor.pagination.feed') }}
+    </div>
     </div>
 
 @endsection

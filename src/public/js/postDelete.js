d96938b6fd9d530d479edbc81d5221b200decabe
jeromes123 <qjjpsoriano@tip.edu.ph>
2024-06-13 16:49:24 +0800

    remove unnecessary comment, used early return approach

--- src/public/js/postDelete.js
+++ src/public/js/postDelete.js
@@ -1,26 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.addEventListener('click', function(event) {
-        // Toggle the dropdown menu
         if (event.target.classList.contains('menu-btn')) {
             const dropdownId = event.target.dataset.dropdownId;
             const dropdownPost = document.getElementById(dropdownId);
             dropdownPost.classList.toggle('hidden');
             dropdownPost.classList.toggle('show');
-        } else {
-            // Close all dropdowns if click is outside
-            document.querySelectorAll('.dropdown-post').forEach(dropdown => {
-                if (!dropdown.classList.contains('hidden')) {
-                    dropdown.classList.add('hidden');
-                    dropdown.classList.remove('show');
-                }
-            });
+            return;
         }
+        
+        document.querySelectorAll('.dropdown-post').forEach(dropdown => {
+            if (!dropdown.classList.contains('hidden')) {
+                dropdown.classList.add('hidden');
+                dropdown.classList.remove('show');
+            }
+        });
     });
 
     document.addEventListener('click', function(event) {
         if (event.target.id === 'delete-btn') {
             event.preventDefault();
             deleteBtn.classList.remove('hidden');
+            return;
         }
     });
 

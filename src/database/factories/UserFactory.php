e5bd0fd8a3ca90976eb3ef13261852d6daa78944
jeromes123 <qjjpsoriano@tip.edu.ph>
2024-06-14 15:34:22 +0800

    enabled login on factory users

--- src/database/factories/UserFactory.php
+++ src/database/factories/UserFactory.php
@@ -2,20 +2,15 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
+use App\Models\Profile;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
 class UserFactory extends Factory
 {
-    /**
-     * The current password being used by the factory.
-     */
-    protected static ?string $password;
-
     /**
      * Define the model's default state.
      */
@@ -37,5 +32,19 @@ class UserFactory extends Factory
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
         ]);
+    }
+
+    /**
+     * Configure the factory.
+     *
+     * @return $this
+     */
+    public function configure(): static
+    {
+        return $this->afterCreating(function (User $user) {
+            Profile::factory()->create([
+                'user_id' => $user->id,
+            ]);
+        });
     }
 }

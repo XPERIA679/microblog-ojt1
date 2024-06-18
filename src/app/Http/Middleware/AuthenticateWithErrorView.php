b592d0eadbc39bf721a9e57448f967697e6df42a
jeromes123 <qjjpsoriano@tip.edu.ph>
2024-06-18 17:42:00 +0800

    prevent going back to feed when logged out

--- src/app/Http/Middleware/AuthenticateWithErrorView.php
+++ src/app/Http/Middleware/AuthenticateWithErrorView.php
@@ -20,7 +20,10 @@ class AuthenticateWithErrorView
             return response()->view('components.errors.login-first', [], 401);
         }
 
-        return $next($request);
+        $response = $next($request);
+        return $response->header('Cache-Control', 'no-cache, no-store, max-age=0, must-revalidate')
+                        ->header('Pragma', 'no-cache')
+                        ->header('Expires', 'Sun, 02 Jan 1990 00:00:00 GMT');
     }
 }
 
